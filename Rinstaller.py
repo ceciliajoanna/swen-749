6c9355b7780e8fd47f3f2d0a8bf9f6bf2df0064b
holmes2 <priyankkapadia89@gmail.com>
2015-02-14 16:13:52 -0500

    First initial commit for test call to shell

---
+++ Rinstaller.py
@@ -0,0 +1,12 @@
+from subprocess import call
+# Check if the computer has python 3 version installed
+# Check if there is an existing R language installed
+# If yes Check for a possibility in which you install for both the version of R
+#If there is some conflict then user has to be notified for the uninstallation and installation.
+#If there is a possibility then prompt the user for the appropriate Rversion and then chack for dependencies
+# If all the dependencies are met then download the source file and start executing the installation process.
+#If all the dependencies are not met then download all the necessary (Assume :Ubuntu for now)
+#If all the dependencies are met then intiate the installation procedure.
+#Inform the user of switching the R version before using it.
+
+call(["ls","-l"])
