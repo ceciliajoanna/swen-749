7b48d48bd537e778beb3ad31bceb4970ee42dd19
ceciliajoanna <jc_joanna@yahoo.com.br>
2015-02-18 19:12:56 -0500

    Created functions to be reused throught the framework

---
+++ utils.py
@@ -0,0 +1,22 @@
+
+
+def open_url(url, pattern):
+	"""" This function returns all the patterns found in a Web page """ 
+	raw_response = urllib2.urlopen(url)
+	charset = raw_response.info().get_param('charset', 'utf8')
+	html_content = raw_response.read().decode(charset)
+	return re.findall(pattern, html_content)
+
+
+
+
+def extract_gz_file(file_name):
+	""" Given the filename of a gzip file, it extracts it"""
+	zip_file = gzip.open(file_name, 'rb')
+	file_content = zip_file.read()
+	zip_file.close()
+
+	uncompressed_file = open(file_name.replace(".gz",""),"wb")
+	uncompressed_file.write(file_content)
+	uncompressed_file.close()
+	
