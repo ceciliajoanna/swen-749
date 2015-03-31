70091e801872233af10ca62b3b51e8c70df55054
ceciliajoanna <jc_joanna@yahoo.com.br>
2015-03-31 14:07:08 -0400

    Created library installer

--- development/cor_comparator.py
+++ development/cor_comparator.py
@@ -1,4 +1,30 @@
 import difflib
-
+import os
+import re
+import csv
 
 difflib.SequenceMatcher(None)
+
+folder_name = "C:\\Users\\Joanna\\Dropbox\\Mestrado\\EUA\\Disciplinas\\2015_Spring\\SWEN-749\\Final Project\\Replicated Papers\\R releases\\"
+latest_version_file = folder_name + "R3\\R-3.1.3\\src\\library\\stats\\src\\cov.c"
+releases = ["R1","R2","R3"]
+
+latest_version_file_content = open(latest_version_file).read()
+print("Generating diff report")
+with open("C:\\Users\\Joanna\\Dropbox\\Mestrado\\EUA\\Disciplinas\\2015_Spring\\SWEN-749\\Final Project\\Replicated Papers\\cor_diff_report.csv", "w", newline='') as csvfile:
+	csv_report = csv.writer(csvfile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+	csv_report.writerow(['Version','Difference Ratio'])
+
+
+	#Iterate over all releases folder
+	for release in releases:
+		release_folder_name = folder_name + release
+		for subdir, dirs, files in os.walk(release_folder_name):
+			for f in files:
+				if f == "cov.c":
+					file_content = open(os.path.join(subdir, f)).read()
+					matcher = difflib.SequenceMatcher(a=latest_version_file_content, b=file_content)
+					r_version = re.search('R-([0-9.a-z]*)',subdir).group(0)
+					if r_version != "R-3.1.3": #ignore latest version because it is going to be equals to 1
+						csv_report.writerow([r_version,str(matcher.ratio())])
+					
--- production/library_installer.py
+++ production/library_installer.py
@@ -1,18 +1,42 @@
 
 # -*- coding: utf-8 -*-
 
-import urllib.request as urllib2
+
+import subprocess
 import re
 import gzip
 import tarfile
+import sys
 from urllib.parse import urljoin
+import urllib.request as urllib2
 from utils import find_pattern, exists
-import subprocess
+from utils import find_library_url
+
 
 
 class LibraryInstaller:
 	
-	def install(self):
+	def install(self, library, library_version, r_version = "R"):
+		library_url = find_library_url(library_name,library_version):
 		file_name = library_url.rsplit('/',1)[1]
 		urllib2.urlretrieve (library_url, file_name)
-		command = ["R", "CMD", "INSTALL", file_name]
+		command = [r_version, "CMD", "INSTALL", file_name]
+
+
+
+
+
+arguments = sys.argv
+
+if len(arguments) == 3:
+	library_name = arguments[1]
+	library_version = arguments[2]
+	library_url = find_library_url(library_name,library_version)
+	if not library_url:
+		print("ERROR: library {0} version {1} not found!".format(library_name,library_version))
+	else:
+		installer = LibraryInstaller()
+		installer.install(library_name, library_version)
+else:
+	print("Usage: >> Library_Name Library_Version R_Version")
+
